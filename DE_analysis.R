0c2722a7d344558e774cbd564f1a709f5b043df3
udiland <udiland@tauex.tau.ac.il>
2022-05-20 10:24:35 +0300

    filter reads

--- DE_analysis.R
+++ DE_analysis.R
@@ -36,3 +36,53 @@ dim(counts)
 # calculate count per million (CPM) in order to normlize counts by library size 
 cpm <- apply(counts,2, function(x) (x/sum(x))*1e6)
 
+# read sample annotation (metadata)
+annot <- read.table("data/sample-annotation.txt", sep = "\t", header = T)
+
+# have a look
+head(annot)
+summary(annot)
+
+#how many samples in each type?
+table(annot$type)
+
+'''
+lesional   normal 
+95       83
+'''
+
+#how many samples? (are the same as number of column in counts?)
+dim(annot)[1]
+
+#are the names the same in counts and annotation?
+sum(colnames(counts) %in% annot$sample_id)
+
+
+#make a vector of sample_id for each condition (normal/)
+samples_normal <- annot$sample_id[annot$type == 'normal'] 
+samples_lesional <- annot$sample_id[annot$type == 'lesional']
+
+# for each condition mark genes that have CPM > 1 in at least 75% of samples
+# names of genes pass the filter in the normal group
+genes2keep_normal <- names(which(rowSums(cpm[, samples_normal] > 1) > length(samples_normal)*0.75))
+
+# how many are there?
+length(genes2keep_normal) 
+# 17880 genes
+
+# names of genes pass the filter in the lesional group
+genes2keep_lesional <- names(which(rowSums(cpm[, samples_lesional] > 1) > length(samples_lesional)*0.75))
+
+# how many are there?
+length(genes2keep_lesional) 
+# 17248 genes
+
+# combine the 2 names and remove duplications
+genes2keep <- unique(c(genes2keep_normal, genes2keep_lesional))
+
+# subset the count table according to the filtering condition
+cpm <- cpm[genes2keep ,]
+
+# how many genes we have left?
+dim(cpm)[1]
+#18286
